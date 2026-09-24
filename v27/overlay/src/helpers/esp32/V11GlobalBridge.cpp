f7813f91f195deae0d086f45a2511efa0579eafc
kevinkok1999 <kevinkok1999@gmail.com>
2026-09-24 20:05:28 +0200

    security(v27): verify EMQX TLS with DigiCert Global Root G2 on 8883

--- v27/overlay/src/helpers/esp32/V11GlobalBridge.cpp
+++ v27/overlay/src/helpers/esp32/V11GlobalBridge.cpp
@@ -15,11 +15,40 @@
 #endif
 
 #ifndef V27_GLOBAL_PORT
-#define V27_GLOBAL_PORT 1883
+#define V27_GLOBAL_PORT 8883
 #endif
 
 V11GlobalBridge v11_global_bridge;
 V11GlobalBridge* V11GlobalBridge::s_instance = nullptr;
+
+// DigiCert Global Root G2. EMQX documents this CA for broker.emqx.io TLS/8883.
+// Root validity extends to 2038; the server hostname is verified by
+// WiFiClientSecure during TLS connection.
+static const char V27_EMQX_ROOT_CA[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIDjjCCAnagAwIBAgIQAzrx5qcRqaC7KGSxHQn65TANBgkqhkiG9w0BAQsFADBh
+MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
+d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBH
+MjAeFw0xMzA4MDExMjAwMDBaFw0zODAxMTUxMjAwMDBaMGExCzAJBgNVBAYTAlVT
+MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j
+b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IEcyMIIBIjANBgkqhkiG
+9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuzfNNNx7a8myaJCtSnX/RrohCgiN9RlUyfuI
+2/Ou8jqJkTx65qsGGmvPrC3oXgkkRLpimn7Wo6h+4FR1IAWsULecYxpsMNzaHxmx
+1x7e/dfgy5SDN67sH0NO3Xss0r0upS/kqbitOtSZpLYl6ZtrAGCSYP9PIUkY92eQ
+q2EGnI/yuum06ZIya7XzV+hdG82MHauVBJVJ8zUtluNJbd134/tJS7SsVQepj5Wz
+tCO7TG1F8PapspUwtP1MVYwnSlcUfIKdzXOS0xZKBgyMUNGPHgm+F6HmIcr9g+UQ
+vIOlCsRnKPZzFBQ9RnbDhxSJITRNrw9FDKZJobq7nMWxM4MphQIDAQABo0IwQDAP
+BgNVHRMBAf8EBTADAQH/MA4GA1UdDwEB/wQEAwIBhjAdBgNVHQ4EFgQUTiJUIBiV
+5uNu5g/6+rkS7QYXjzkwDQYJKoZIhvcNAQELBQADggEBAGBnKJRvDkhj6zHd6mcY
+1Yl9PMWLSn/pvtsrF9+wX3N3KjITOYFnQoQj8kVnNeyIv/iPsGEMNKSuIEyExtv4
+NeF22d+mQrvHRAiGfzZ0JFrabA0UWTW98kndth/Jsw1HKj2ZL7tcu7XUIOGZX1NG
+Fdtom/DzMNU+MeKNhJ7jitralj41E6Vf8PlwUHBHQRFXGU7Aj64GxJUTFy8bJZ91
+8rGOmaFvE7FBcf6IKshPECBV1/MUReXgRPTqh5Uykw7+U0b6LJ3/iyK5S9kJRaTe
+pLiaWN0bfVKfjllDiIGknibVb63dDcY3fe0Dkhvld1927jyNxF1WW6LZZm6zNTfl
+MrY=
+-----END CERTIFICATE-----
+)EOF";
+
 
 namespace {
 bool sha256(const uint8_t* data, size_t len, uint8_t out[32]) {
@@ -51,6 +80,8 @@ void V11GlobalBridge::begin(MyMesh* mesh) {
     s_instance = this;
     memcpy(_selfPub, _mesh->getSelfPubKey(), sizeof(_selfPub));
 
+    // Fail closed: verified TLS only. Never call setInsecure() in V27.
+    _wc.setCACert(V27_EMQX_ROOT_CA);
     _mqtt.setServer(V27_GLOBAL_BROKER, V27_GLOBAL_PORT);
     _mqtt.setKeepAlive(45);
     _mqtt.setSocketTimeout(2);
